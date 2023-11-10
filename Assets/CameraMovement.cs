0c7846aedb70d7fda1ed9f02d4b85adf8692337d
MoriartyCoder <dantronnier@hotmail.de>
2023-11-10 15:55:52 +0100

    More movements added; With "E" you can oversee the field

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -6,27 +7,28 @@ public class CameraMovement : MonoBehaviour
     public float verticalSpeed = 3f; // Geschwindigkeit der vertikalen Kamerabewegung
     public float rotationSpeed = 3f; // Geschwindigkeit der Kameradrehung
     public float shiftMultiplier = 2f; // Multiplikator für die Geschwindigkeit bei Umschalttaste
+    public Transform topDownPosition; // Zielposition für die Top-Down-Ansicht
+
+    private bool isMovingToTopDown = false; // Flag, um zu überprüfen, ob die Kamera in die Top-Down-Ansicht bewegt wird
 
     void Update()
     {
-        // Tastatureingaben erfassen
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // Kamerabewegung berechnen
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
-
-        // Leertaste prüfen (nach oben bewegen)
-        if (Input.GetKey(KeyCode.Space))
+        // Wenn die Taste "E" gedrückt wird, startet die Coroutine für die Bewegung zur Top-Down-Ansicht
+        if (Input.GetKeyDown(KeyCode.E) && !isMovingToTopDown)
         {
-            transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime);
+            StartCoroutine(MoveToTopDownView());
         }
 
-        // Linke Umschalttaste prüfen (nach unten bewegen)
+        // Umschalttaste prüfen
+        float multiplier = 1f;
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime);
+            multiplier = shiftMultiplier;
         }
+
+        // Kamera bewegen
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        transform.Translate(movement * multiplier);
 
         // Mausdruck prüfen (linke Maustaste)
         if (Input.GetMouseButton(0))
@@ -39,15 +41,26 @@ public class CameraMovement : MonoBehaviour
             transform.Rotate(Vector3.up * rotationX, Space.World);
             transform.Rotate(Vector3.right * rotationY, Space.Self);
         }
+    }
 
-        // Umschalttaste prüfen
-        float multiplier = 1f;
-        if (Input.GetKey(KeyCode.LeftShift))
+    // Coroutine für die Bewegung zur Top-Down-Ansicht
+    IEnumerator MoveToTopDownView()
+    {
+        isMovingToTopDown = true;
+
+        // Nach Erreichen der Top-Down-Position, schaue nach unten
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+
+        // Zielposition festlegen (beispielsweise über der Szene)
+        Vector3 targetPosition = topDownPosition.position;
+
+        // Kamera langsam zur Zielposition bewegen
+        while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
         {
-            multiplier = shiftMultiplier;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 2f);
+            yield return null;
         }
 
-        // Kamera bewegen
-        transform.Translate(movement * multiplier);
+        isMovingToTopDown = false;
     }
 }
